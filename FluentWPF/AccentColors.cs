c8cd02f73f1a7e9b9e38ab895378e71e8a1f050c
minami <sourcechord@gmail.com>
2017-11-13 00:27:09 +0900

    Add AccentColors class

---
+++ FluentWPF/AccentColors.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace SourceChord.FluentWPF
+{
+    public class AccentColors
+    {
+        [DllImport("uxtheme.dll", EntryPoint = "#95", CharSet = CharSet.Unicode)]
+        internal static extern uint GetImmersiveColorFromColorSetEx(uint dwImmersiveColorSet, uint dwImmersiveColorType, bool bIgnoreHighContrast, uint dwHighContrastCacheMode);
+        [DllImport("uxtheme.dll", EntryPoint = "#96", CharSet = CharSet.Unicode)]
+        internal static extern uint GetImmersiveColorTypeFromName(string name);
+        [DllImport("uxtheme.dll", EntryPoint = "#98", CharSet = CharSet.Unicode)]
+        internal static extern uint GetImmersiveUserColorSetPreference(bool bForceCheckRegistry, bool bSkipCheckOnFail);
+
+        public static Color GetColorByTypeName(string name)
+        {
+            var colorSet = GetImmersiveUserColorSetPreference(false, false);
+            var colorType = GetImmersiveColorTypeFromName(name);
+            var rawColor = GetImmersiveColorFromColorSetEx(colorSet, colorType, false, 0);
+
+            var bytes = BitConverter.GetBytes(rawColor);
+            return Color.FromArgb(bytes[3], bytes[0], bytes[1], bytes[2]);
+        }
+
+        public static Color SystemAccentColor { get; private set; }
+
+        static AccentColors()
+        {
+            SystemAccentColor = GetColorByTypeName("ImmersiveStartBackground");
+        }
+    }
+}

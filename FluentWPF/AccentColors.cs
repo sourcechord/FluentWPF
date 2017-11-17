ccef7c5c5a15185063e987f0392a686e1adcea70
minami <sourcechord@gmail.com>
2017-11-17 21:59:30 +0900

    Add AccentColors class

--- FluentWPF/AccentColors.cs
+++ FluentWPF/AccentColors.cs
@@ -29,9 +29,25 @@ namespace SourceChord.FluentWPF
 
         public static Color SystemAccentColor { get; private set; }
 
+        public static Color ImmersiveSystemAccent { get; private set; }
+        public static Color ImmersiveSystemAccentDark1 { get; private set; }
+        public static Color ImmersiveSystemAccentDark2 { get; private set; }
+        public static Color ImmersiveSystemAccentDark3 { get; private set; }
+        public static Color ImmersiveSystemAccentLight1 { get; private set; }
+        public static Color ImmersiveSystemAccentLight2 { get; private set; }
+        public static Color ImmersiveSystemAccentLight3 { get; private set; }
+
         static AccentColors()
         {
             SystemAccentColor = GetColorByTypeName("ImmersiveStartBackground");
+
+            ImmersiveSystemAccent = GetColorByTypeName("ImmersiveSystemAccent");
+            ImmersiveSystemAccentDark1 = GetColorByTypeName("ImmersiveSystemAccentDark1");
+            ImmersiveSystemAccentDark2 = GetColorByTypeName("ImmersiveSystemAccentDark2");
+            ImmersiveSystemAccentDark3 = GetColorByTypeName("ImmersiveSystemAccentDark3");
+            ImmersiveSystemAccentLight1 = GetColorByTypeName("ImmersiveSystemAccentLight1");
+            ImmersiveSystemAccentLight2 = GetColorByTypeName("ImmersiveSystemAccentLight2");
+            ImmersiveSystemAccentLight3 = GetColorByTypeName("ImmersiveSystemAccentLight3");
         }
     }
 }

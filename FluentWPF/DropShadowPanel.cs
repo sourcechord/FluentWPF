61aee446b6c551c0bdc57885bad0774a9dd14b2d
minami <sourcechord@gmail.com>
2018-01-07 20:04:55 +0900

    Add ShadowMode property to DropShadowPanel

--- FluentWPF/DropShadowPanel.cs
+++ FluentWPF/DropShadowPanel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,13 @@ using System.Windows.Shapes;
 
 namespace SourceChord.FluentWPF
 {
+    public enum ShadowMode
+    {
+        Content = 0,
+        Inner,
+        Outer,
+    }
+
     public class DropShadowPanel : Decorator
     {
 
@@ -86,6 +94,19 @@ namespace SourceChord.FluentWPF
             DependencyProperty.Register("ShadowDepth", typeof(double), typeof(DropShadowPanel), new PropertyMetadata(0.0));
 
 
+
+
+        public ShadowMode ShadowMode
+        {
+            get { return (ShadowMode)GetValue(ShadowModeProperty); }
+            set { SetValue(ShadowModeProperty, value); }
+        }
+        // Using a DependencyProperty as the backing store for ShadowMode.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ShadowModeProperty =
+            DependencyProperty.Register("ShadowMode", typeof(ShadowMode), typeof(DropShadowPanel), new PropertyMetadata(ShadowMode.Content));
+
+
+
         private ContainerVisual _internalVisual;
 
         public DropShadowPanel()
@@ -157,18 +178,103 @@ namespace SourceChord.FluentWPF
             BindingOperations.SetBinding(effect, DropShadowEffect.OpacityProperty, new Binding("ShadowOpacity") { Source = this });
             BindingOperations.SetBinding(effect, DropShadowEffect.RenderingBiasProperty, new Binding("RenderingBias") { Source = this });
             BindingOperations.SetBinding(effect, DropShadowEffect.ShadowDepthProperty, new Binding("ShadowDepth") { Source = this });
-            var border = new Rectangle()
-            {
-                Effect = effect
-            };
+
+            // DropShadowのモード切替用にトリガーを設定
+            var st = new Style();
+
+            // ShadowMode.Contentの場合は、ContentをVisualBrushとして扱ってContentの形状に応じた影を作る
             var brush = new VisualBrush(value)
             {
                 TileMode = TileMode.None,
-                Stretch = Stretch.None
+                Stretch = Stretch.None,
+                AlignmentX = AlignmentX.Left,
+                AlignmentY = AlignmentY.Top
             };
-            BindingOperations.SetBinding(border, Rectangle.FillProperty, new Binding("Background") { Source = this, TargetNullValue = brush });
+            var contentTrigger = new DataTrigger()
+            {
+                Binding = new Binding("ShadowMode") { Source = this },
+                Value = ShadowMode.Content,
+            };
+            contentTrigger.Setters.Add(new Setter()
+            {
+                Property = Rectangle.FillProperty,
+                Value = brush
+            });
+            st.Triggers.Add(contentTrigger);
+
+
+            // ShadowMode.Outerの場合は、影はコントロールの外側にだけ表示
+            var outerTrigger = new DataTrigger()
+            {
+                Binding = new Binding("ShadowMode") { Source = this },
+                Value = ShadowMode.Outer,
+            };
+            outerTrigger.Setters.Add(new Setter()
+            {
+                Property = Rectangle.ClipProperty,
+                Value = new MultiBinding()
+                {
+                    Bindings =
+                    {
+                        new Binding("ActualWidth") { Source = this },
+                        new Binding("ActualHeight") { Source = this },
+                        new Binding("BlurRadius") { Source = this },
+                    },
+                    Converter = new ClipInnerRectConverter()
+                }
+            });
+            outerTrigger.Setters.Add(new Setter()
+            {
+                Property = Rectangle.FillProperty,
+                Value = Brushes.White
+            });
+            st.Triggers.Add(outerTrigger);
+
+            // ShadowMode.Innerの場合は、影はコントロールの内側にだけ表示
+            var innerTrigger = new DataTrigger()
+            {
+                Binding = new Binding("ShadowMode") { Source = this },
+                Value = ShadowMode.Inner,
+            };
+            innerTrigger.Setters.Add(new Setter()
+            {
+                Property = Rectangle.StrokeProperty,
+                Value = Brushes.White
+            });
+            innerTrigger.Setters.Add(new Setter()
+            {
+                Property = Rectangle.StrokeThicknessProperty,
+                Value = new Binding("BlurRadius") { Source = this }
+            });
+            innerTrigger.Setters.Add(new Setter()
+            {
+                Property = Rectangle.MarginProperty,
+                Value = new Binding("BlurRadius") { Source = this, Converter = new NegativeMarginConverter() }
+            });
+            innerTrigger.Setters.Add(new Setter()
+            {
+                Property = Rectangle.ClipProperty,
+                Value = new MultiBinding()
+                {
+                    Bindings =
+                    {
+                        new Binding("ActualWidth") { Source = this },
+                        new Binding("ActualHeight") { Source = this },
+                        new Binding("BlurRadius") { Source = this },
+                    },
+                    Converter = new ClipOuterRectConverter()
+                }
+            });
+            st.Triggers.Add(innerTrigger);
+
+            var border = new Rectangle()
+            {
+                Effect = effect,
+                Style = st,
+            };
 
             var grid = new Grid();
+            BindingOperations.SetBinding(grid, Grid.BackgroundProperty, new Binding("Background") { Source = this });
             grid.Children.Add(border);
             grid.Children.Add(value);
 
@@ -185,6 +291,66 @@ namespace SourceChord.FluentWPF
                 throw new ArgumentOutOfRangeException();
             }
             return this.InternalVisual;
+        }
+    }
+
+
+    internal class ClipInnerRectConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values.Any(o => o == DependencyProperty.UnsetValue || o == null)) return null;
+
+            var width = (double)values[0];
+            var height = (double)values[1];
+            var outerMargin = (double)values[2];
+
+            var region = new RectangleGeometry(new Rect(-outerMargin, -outerMargin, width + (outerMargin * 2), height + (outerMargin * 2)));
+            var clip = new RectangleGeometry(new Rect(0, 0, width, height));
+
+            var group = new GeometryGroup();
+            group.Children.Add(region);
+            group.Children.Add(clip);
+
+            return group;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class ClipOuterRectConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values.Any(o => o == DependencyProperty.UnsetValue || o == null)) return null;
+
+            var width = (double)values[0];
+            var height = (double)values[1];
+            var margin = (double)values[2];
+
+            return new RectangleGeometry(new Rect(margin, margin, width, height));
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class NegativeMarginConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var margin = (double)value;
+            return new Thickness(-margin);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
         }
     }
 }

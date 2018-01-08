08b5082b10afc8e7c17c7cd594a087f12c855daa
minami <sourcechord@gmail.com>
2018-01-08 10:56:26 +0900

    v0.2.0

--- FluentWPF/Properties/AssemblyInfo.cs
+++ FluentWPF/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]

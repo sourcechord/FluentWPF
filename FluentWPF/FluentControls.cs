3475f634b48ee14ecf9eacc5e896c975d2992eaa
Fernando Angeles Jr <fernandoangelesjr_02@live.com>
2018-10-31 09:50:21 +0800

    Test attached property on text box

---
+++ FluentWPF/FluentControls.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace SourceChord.FluentWPF
+{
+    public class FluentControls : FrameworkElement
+    {
+
+        public static void SetHeadertext(UIElement element, string value)
+        {
+            element.SetValue(HeadertextProperty, value);
+        }
+        public static string GetHeadertext(UIElement element)
+        {
+            return (string)element.GetValue(HeadertextProperty);
+        }
+
+
+        public static readonly DependencyProperty HeadertextProperty = DependencyProperty.RegisterAttached("Headertext", typeof(string), typeof(FluentControls), new PropertyMetadata(""));
+
+
+
+
+
+    }
+}

7cd35374a72eff338ff66fba391045f3f55e171d
sourcechord <sourcechord@gmail.com>
2019-08-31 23:22:30 +0900

    Update ButtonRevealStyle
    * Add RevealElement.HoverBrush attached property
    * Share ButtonRevealStyle implements with ButtonAccentRevealStyle

--- FluentWPF/PointerTracker.cs
+++ FluentWPF/PointerTracker.cs
@@ -236,4 +236,19 @@ namespace SourceChord.FluentWPF
             return brush;
         }
     }
+
+    public class RevealElement
+    {
+        public static Brush GetHoverBrush(DependencyObject obj)
+        {
+            return (Brush)obj.GetValue(HoverBrushProperty);
+        }
+        public static void SetHoverBrush(DependencyObject obj, Brush value)
+        {
+            obj.SetValue(HoverBrushProperty, value);
+        }
+        // Using a DependencyProperty as the backing store for HoverBrush.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty HoverBrushProperty =
+            DependencyProperty.RegisterAttached("HoverBrush", typeof(Brush), typeof(RevealElement), new FrameworkPropertyMetadata(Brushes.Blue, FrameworkPropertyMetadataOptions.Inherits));
+    }
 }

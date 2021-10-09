4f90b74b67f25bea3da3c5a499a080bb30251210
sourcechord <sourcechord@gmail.com>
2021-10-09 13:07:06 +0900

    Fix #82

--- FluentWPF/Extensions.cs
+++ FluentWPF/Extensions.cs
@@ -81,8 +81,67 @@ namespace SourceChord.FluentWPF
             return (Brush)element.GetValue(PlaceholderForegroundBrushProperty);
         }
 
+
+
+
+        #region PasswordBox Attached Prperties
+
+        internal static bool GetIsPasswordBoxExtensionAttached(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(IsPasswordBoxExtensionAttachedProperty);
+        }
+        internal static void SetIsPasswordBoxExtensionAttached(DependencyObject obj, bool value)
+        {
+            obj.SetValue(IsPasswordBoxExtensionAttachedProperty, value);
+        }
+        // Using a DependencyProperty as the backing store for IsPasswordBoxExtensionAttached.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsPasswordBoxExtensionAttachedProperty =
+            DependencyProperty.RegisterAttached("IsPasswordBoxExtensionAttached", typeof(bool), typeof(Extensions), new PropertyMetadata(false, OnIsPasswordBoxExtensionAttachedChanged));
+
+        private static void OnIsPasswordBoxExtensionAttachedChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var ctrl = d as PasswordBox;
+            var newValue = (bool)e.NewValue;
+            var oldValue = (bool)e.OldValue;
+            if (ctrl == null) return;
+
+            // 無効になった場合の処理
+            if (oldValue && !newValue)
+            {
+                ctrl.PasswordChanged -= Ctrl_PasswordChanged;
+            }
+
+            // 有効になった場合の処理
+            if (!oldValue && newValue)
+            {
+                ctrl.PasswordChanged += Ctrl_PasswordChanged;
+                var value = string.IsNullOrEmpty(ctrl.Password);
+                SetIsPasswordEmpty(ctrl, value);
+            }
+        }
+
+        private static void Ctrl_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            var ctrl = sender as PasswordBox;
+            if (ctrl != null)
+            {
+                var value = string.IsNullOrEmpty(ctrl.Password);
+                SetIsPasswordEmpty(ctrl, value);
+            }
+        }
+
+        public static bool GetIsPasswordEmpty(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(IsPasswordEmptyProperty);
+        }
+        private static void SetIsPasswordEmpty(DependencyObject obj, bool value)
+        {
+            obj.SetValue(IsPasswordEmptyProperty, value);
+        }
+        // Using a DependencyProperty as the backing store for IsPasswordEmpty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsPasswordEmptyProperty =
+            DependencyProperty.RegisterAttached("IsPasswordEmpty", typeof(bool), typeof(Extensions), new PropertyMetadata(true));
+
+        #endregion
     }
-
-
-
 }
